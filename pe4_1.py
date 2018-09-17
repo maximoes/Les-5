bf2f09141d17cc8becd07b6f3f6c92b99f2d018e
Maxime <Maxime.salero@student.hu.nl>
2018-09-17 22:29:29 +0200

    Opdracht programming

---
+++ pe4_1.py
@@ -0,0 +1,5 @@
+
+uurloon = float(input(' wat verdien je per uur '))
+aantalUur = int(input(' hoeveel uur heb je gewerkt '))
+salaris = (uurloon * aantalUur)
+print(str(aantalUur) + ' uur werken levert ' + str(salaris) + ' Euro op ')
